921e1c5896dd385d87829ecb8972b07f83aa2450
Seremba <pserembae@gmail.com>
2022-06-13 13:08:17 +0300

    Add main section and bottom section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Header from './components/Header'
+import MenuContainer from './components/MenuContainer'
 import './App.css';
+import { HomeRounded, Chat, AccountBalanceWalletRounded, Favorite, SummarizeRounded, Settings } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
+      {/* header section  */}
       <Header />
+      {/* main section  */}
+      <main>
+        <div className="mainContainer"></div>
+        <div className="righ-menu"></div>
+      </main>
+      {/* bottom menu  */}
+      <div className="bottomMenu">
+        <ul id="menu">
+          <MenuContainer link={'#'} icon={ <HomeRounded/>} />
+          <MenuContainer link={'#'} icon={ <Chat />} />
+          <MenuContainer link={'#'} icon={ <AccountBalanceWalletRounded/>} />
+          <MenuContainer link={'#'} icon={ <Favorite/>} />
+          <MenuContainer link={'#'} icon={ <SummarizeRounded/>} />
+          <MenuContainer link={'#'} icon={ <Settings/>} />
+
+          <div className="indicator"></div>
+        </ul>
+      </div>
     </div>
   );
 }
